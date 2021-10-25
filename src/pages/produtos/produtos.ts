cc7097e5b1520646ca5aeab93ba0b289c83479c2
Nadilson <nadilson@protonmail.com>
2021-10-25 05:10:05 -0300

    Infinite scroll

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { IonicPage, LoadingController, NavController, NavParams } from 'ionic-angular';
 import { ProdutoService } from '../../services/domain/produto.service';
 import { API_CONFIG } from '../../config/api.config';
+import { convertCompilerOptionsFromJson } from 'typescript';
 
 @IonicPage()
 @Component({
@@ -11,7 +12,8 @@ import { API_CONFIG } from '../../config/api.config';
 })
 export class ProdutosPage {
 
-  items : ProdutoDTO[];
+  items : ProdutoDTO[] = [];
+  page : number = 0;
 
 
   constructor(
@@ -28,45 +30,59 @@ export class ProdutosPage {
   loadData() {
     let categoria_id = this.navParams.get('categoria_id');
     let loader = this.presentLoading();
-    this.produtoService.findByCategoria(categoria_id)
-        .subscribe(response => {
-          this.items = response['content'];
-          loader.dismiss();
-          this.loadImageUrls();
+    this.produtoService.findByCategoria(categoria_id, this.page, 10)
+          .subscribe(response => {
+            let start = this.items.length;
+            this.items = this.items.concat(response['content']);
+            let end = this.items.length - 1;
+            loader.dismiss();
+            console.log(this.page);
+            console.log(this.items);
+            this.loadImageUrls(start, end);
         },
         error => {
           loader.dismiss();
         });
   }
   
-     loadImageUrls() {
-       for (var i=0; i<this.items.length; i++) {
-         let item = this.items[i];
-         this.produtoService.getImageFromBucket(item.id)
-             .subscribe(response => {
-               item.imageUrl = `${API_CONFIG.bucketBaseUrl}/prod${item.id}-small.jpg`;               
-             },
-             error => {});
-        }      
-     }
-
-     showDetail(produto_id : string) {
-       this.navCtrl.push('ProdutoDetailPage', {produto_id : produto_id});
-     }
-
-     presentLoading() {
-       let loader = this.loadingCtrl.create({
-         content: "Aguarde..."
-       });
-       loader.present();
-       return loader;
-     }
-
-     doRefresh(refresher) {
-      this.loadData();
-      setTimeout(() => {
-        refresher.complete();
-      }, 1000);
+  loadImageUrls(start: number, end: number) {
+    for (var i=start; i<=end; i++) {
+      let item = this.items[i];
+      this.produtoService.getImageFromBucket(item.id)
+        .subscribe(response => {
+          item.imageUrl = `${API_CONFIG.bucketBaseUrl}/prod${item.id}-small.jpg`;
+        },
+        error => {});
     }
+  }
+
+  showDetail(produto_id : string) {
+    this.navCtrl.push('ProdutoDetailPage', {produto_id : produto_id});
+  }
+
+  presentLoading() {
+    let loader = this.loadingCtrl.create({
+      content: "Aguarde..."
+    });
+    loader.present();
+    return loader;
+  }
+  
+  doRefresh(refresher) {
+    this.page = 0;
+    this.items = [];
+    this.loadData();
+    setTimeout(() => {
+      refresher.complete();
+    }, 1000);
+  }
+  
+  doInfinite(infiniteScroll) {
+    this.page++;
+    this.loadData();
+    setTimeout(() => {
+      infiniteScroll.complete();
+    }, 1000);
+  }
 
 }
