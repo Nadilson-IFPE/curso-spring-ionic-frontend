a02f54b220433f785ca4ddc196c9d69293e56317
Nadilson <nadilson@protonmail.com>
2021-11-03 09:28:03 -0300

    Testando o app no dispositivo

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,4 +1,4 @@
 export const API_CONFIG = {
-    baseUrl: "http://localhost:8080",
+    baseUrl: "https://nadilson-curso-spring-ionic.herokuapp.com",
     bucketBaseUrl: "https://s3.sa-east-1.amazonaws.com/nadilson-curso-spring-ionic"
 }
